ca884d8c2373947a6e73f78f0bcb68ad690ad972
Guille Benitez <guillehbenitez@gmail.com>
2025-10-04 15:26:01 -0300

    Update. Pantalla principal, restan detalles

--- app/src/main/java/com/example/clubdeportivo13/PrincipalActivity.kt
+++ app/src/main/java/com/example/clubdeportivo13/PrincipalActivity.kt
@@ -1,20 +1,32 @@
 package com.example.clubdeportivo13
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class PrincipalActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_principal)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+            val btnGestionUsuarios = findViewById<Button>(R.id.btnGestionUsuarios)
+
+            btnGestionUsuarios.setOnClickListener {
+                val intent = Intent(this, GestionUsuariosActivity::class.java)
+                startActivity(intent)
+            }
+
+            val btnMorososHoy = findViewById<Button>(R.id.btnMorososHoy)
+
+            btnMorososHoy.setOnClickListener {
+                val intent = Intent(this, ListadoMorososActivity::class.java)
+                startActivity(intent)
+            }
+
     }
 }
