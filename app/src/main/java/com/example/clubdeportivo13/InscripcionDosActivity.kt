83c392b89600a6a85792ccbf392cf352184ed7e3
mdev <mdev.repository@gmail.com>
2025-10-05 16:55:14 -0300

    Update. Creacion de pantalla Inscripcion parte 2. Pendiente botonera general

--- app/src/main/java/com/example/clubdeportivo13/InscripcionDosActivity.kt
+++ app/src/main/java/com/example/clubdeportivo13/InscripcionDosActivity.kt
@@ -1,20 +1,44 @@
 package com.example.clubdeportivo13
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class InscripcionDosActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_inscripcion_dos)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnSocio = findViewById<Button>(R.id.btnSocioSelec)
+        val btnNoSocio = findViewById<Button>(R.id.btnNoSocioSelec)
+
+        val btnAnterior = findViewById<Button>(R.id.btnAnteriorInsDos)
+        val btnCanlear = findViewById<Button>(R.id.btnCancelarInsDos)
+
+        btnSocio.setOnClickListener {
+            val intent = Intent(this, PagarCuotaActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnNoSocio.setOnClickListener {
+            val intent = Intent(this, PagarActividadActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnAnterior.setOnClickListener {
+            val intent = Intent(this, InscripcionUnoActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnCanlear.setOnClickListener {
+            val intent = Intent(this, GestionNoRegActivity::class.java)
+            startActivity(intent)
         }
     }
 }
