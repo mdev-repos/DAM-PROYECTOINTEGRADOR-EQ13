b73b9edab4213d1fd731b94c696e7f62b68e2ea6
mdev <mdev.repository@gmail.com>
2025-10-05 15:55:43 -0300

    Update. Se construye la pantalla Inscripcion Uno. Se hace la conexion con las dos pantallas establecidas. Pendiente: Botonera especial

--- app/src/main/java/com/example/clubdeportivo13/InscripcionUnoActivity.kt
+++ app/src/main/java/com/example/clubdeportivo13/InscripcionUnoActivity.kt
@@ -1,20 +1,30 @@
 package com.example.clubdeportivo13
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class InscripcionUnoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_inscripcion_uno)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnSiguiente = findViewById<Button>(R.id.btnSiguienteInsUno)
+
+        btnSiguiente.setOnClickListener {
+            val intent = Intent(this, InscripcionDosActivity::class.java)
+            startActivity(intent)
         }
+
+        val btnCancelar = findViewById<Button>(R.id.btnCancelarInsUno)
+
+        btnCancelar.setOnClickListener {
+            val intent = Intent(this, GestionNoRegActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
