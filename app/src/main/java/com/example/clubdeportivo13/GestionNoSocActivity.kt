b12e2e4dc01b764387aafc4b414ea5f86ba4ad9e
osposto <osposto@gmail.com>
2025-10-05 21:41:14 -0300

    CCCCs

--- app/src/main/java/com/example/clubdeportivo13/GestionNoSocActivity.kt
+++ app/src/main/java/com/example/clubdeportivo13/GestionNoSocActivity.kt
@@ -13,7 +13,7 @@ class GestionNoSocActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_listado_morosos)
+        setContentView(R.layout.activity_gestion_no_soc)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
