f7b826805904b36ccb4caa4313a82dfa85a05e37
mdev <mdev.repository@gmail.com>
2025-10-05 21:54:33 -0300

    Update. Se actualiza en menu botonera global de las pantallas GestionNoRegistrados, GestionUsuarios, Inscripcion Uno y Dos y Pantalla Principal

--- app/src/main/java/com/example/clubdeportivo13/GestionUsuariosActivity.kt
+++ app/src/main/java/com/example/clubdeportivo13/GestionUsuariosActivity.kt
@@ -1,9 +1,11 @@
 package com.example.clubdeportivo13
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -48,6 +50,41 @@ class GestionUsuariosActivity : AppCompatActivity() {
             val intent = Intent(this, GestionUsuariosActivity::class.java)
             startActivity(intent)
 
+        }
+
+        val btnCerrarSecion = findViewById<ImageButton>(R.id.IconButton1)
+
+        btnCerrarSecion.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle("Cerrar Sesión")
+                .setMessage("¿Estás seguro?")
+                .setPositiveButton("Sí") { dialog, which ->
+
+                    val prefs = getSharedPreferences("app_prefs", MODE_PRIVATE)
+                    prefs.edit().putBoolean("is_logged_in", false).apply()
+
+
+                    val intent = Intent(this, LoginActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(intent)
+                    finish()
+                }
+                .setNegativeButton("Cancelar", null)
+                .show()
+        }
+
+        val btnPantallaPrincipal = findViewById<ImageButton>(R.id.IconButton2)
+
+        btnPantallaPrincipal.setOnClickListener {
+            val intent = Intent(this, PrincipalActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnVolverPantalla = findViewById<ImageButton>(R.id.IconButton3)
+
+        btnVolverPantalla.setOnClickListener {
+            val intent = Intent(this, PrincipalActivity::class.java)
+            startActivity(intent)
         }
     }
 }
